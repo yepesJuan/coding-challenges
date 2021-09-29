d636d842a7638fa37ea5447782d2f6df00e9aa49
Juan Yepes <yepes.j@hotmail.com>
2021-09-28 20:27:57 -0400

    added some challenges

--- halfPyramid.js
+++ halfPyramid.js
@@ -1,11 +1,9 @@
-//  Inside of the homework folder, create another file named function and do this:
 
-// 1. Write a function that adds two numbers together
-
-console.log("lol")
-
-function addTwoNumbers(a,b) {
-    return a + b 
-}
-let result = addTwoNumbers(10,12)
-console.log(result)
+   function outputPyramid(rows,character){
+    let row = character
+    for( let i = 0; i < rows ; i++){
+      console.log(row)
+      row += character
+    }
+  }
+  outputPyramid(5,"#") 
---
+++ hw.js
@@ -0,0 +1,19 @@
+/*
+1. implement enqueue()
+- Make an array of numbers 
+- Write a function that inserts a number to the tail with two arguments
+
+2. implement dequeue()
+Write a function to delete the first num of the array. */
+
+let arr = [3,4,5,16,7,8,2,6,1]
+
+function enqueue(){
+ // not done
+}
+
+function dequeue(){
+ // not done
+}
+
+
