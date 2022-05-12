b3dfc8fa6bd34573b709cd569790333dcab0b0b7
yepesJuan <yepes.j@hotmail.com>
2022-05-12 10:14:16 -0400

    new sqrt root

--- squareRoot.js
+++ squareRoot.js
@@ -1,7 +1,3 @@
-const num = 2;
-// 9 -> 3
-// 16 -> 4
-
 const squareRoot = (num) => {
   if (num === 1) {
     return 1;
@@ -15,4 +11,32 @@ const squareRoot = (num) => {
     }
 };
 
-console.log(squareRoot(num));
+console.log(squareRoot(2));
+
+const square = (n, i, j) => {
+  let mid = (i + j) / 2;
+  let mul = mid * mid;
+  if (mul === n || Math.abs(mul - n) < 0.00001) {
+    return mid;
+  } else if (mul < n) {
+    return square(n, mid, j);
+  } else {
+    return square(n, i, mid);
+  }
+};
+// Function to find the square root of n
+const findSqrt = (num) => {
+  let i = 1;
+  const found = false;
+  while (!found) {
+    // If n is a perfect square
+    if (i * i === num) {
+      return i;
+    } else if (i * i > num) {
+      let res = square(num, i - 1, i);
+      return res;
+    }
+    i++;
+  }
+};
+console.log(findSqrt(33));
