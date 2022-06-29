e2278b36c18720988483cb40a20791877617be37
yepesJuan <yepes.j@hotmail.com>
2022-06-28 20:36:06 -0400

    updates to some challenges

--- evensPlease.js
+++ evensPlease.js
@@ -1,5 +1,5 @@
 const arr = [1, 4, 6, 9, 2];
-
+// return
 function evenPls(array) {
   const newArr = [];
   for (let i = 0; i < array.length; i++) {
@@ -11,5 +11,19 @@ function evenPls(array) {
 }
 console.log(evenPls(arr));
 
+// method
 const evenArr = arr.filter((x) => x % 2 === 0);
-console.log(evenArr);
+console.log({ evenArr });
+
+// no return
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+function evensOnly(nums) {
+  for (let i = 0; i < nums.length; i++) {
+    // console.log(nums[i] % 2 === 0);
+    if (nums[i] % 2 === 0) {
+      console.log(nums[i]);
+    }
+  }
+}
+
+console.log(evensOnly(nums));
--- removeDuplicates.js
+++ removeDuplicates.js
@@ -1,13 +1,13 @@
 const numbers = [10, 10, 20, 30, 30, 20];
 const filteredNumbers = numbers.filter((number, index) => {
-  console.log(index);
-  // console.log(number);
-  console.log(numbers.indexOf(number));
+  console.log({ number });
+  console.log({ index });
+  console.log(" ", numbers.indexOf(number));
   let ans = numbers.indexOf(number) == index;
   return ans;
   // return ans;
 });
-console.log(filteredNumbers);
+console.log({ filteredNumbers });
 
-const noDuplicates = [...new Set(numbers)];
-console.log(filteredNumbers);
+// const noDuplicates = [...new Set(numbers)];
+// console.log(filteredNumbers);
