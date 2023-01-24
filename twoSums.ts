490b5fc43f711e5ddebd5584b13a506455316bee
Juan Yepes <yepes.j@hotmail.com>
2023-01-24 18:55:09 -0500

    ts fix for two sum

--- twoSums.ts
+++ twoSums.ts
@@ -2,24 +2,36 @@
 // find the only two numbers in the array
 // that add up to the target value
 
-const target = 7;
-const array = [2, 6, 3, 1];
+const target = 17;
+const array = [2, 6, 3, 1, 0, 8, 9, 3];
 
-const fastTwoSum = (arr: number[], _target: number) => {
+const fastTwoSum = (arr: number[], _target: number): void => {
   const map: Record<number, number> = {};
   for (let i = 0; i < arr.length; i++) {
     const difference = _target - arr[i];
     if (map[difference] !== undefined) {
       console.log(arr[map[difference]], "+", arr[i], "=", _target); // arr[1] + arr[3] = target
-    } else {
-      map[arr[i]] = i;
     }
+    map[arr[i]] = i;
   }
 };
 console.time();
 fastTwoSum(array, target);
 console.timeEnd();
-
+/************************************************************************** */
+function fastTwoSum2(nums: number[], target: number) {
+  const map = new Map<number, number>();
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (map.has(complement)) {
+      return [map.get(complement), i];
+    }
+    map.set(nums[i], i);
+  }
+  throw new Error("No two sum solution");
+}
+console.log(fastTwoSum2(array, target));
+/************************************************************************** */
 const twoSum = (arr: number[], target: number) => {
   const result: number[] = [];
   for (let i = 0; i < arr.length; i++) {
@@ -32,6 +44,8 @@ const twoSum = (arr: number[], target: number) => {
   }
   return result;
 };
-twoSum(array, target);
+console.time();
+console.log(twoSum(array, target));
+console.timeEnd();
 
 export {};
