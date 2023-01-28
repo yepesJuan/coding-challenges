d22b4b9db45d1fd79a54cef9466a66eb0954f4cf
Juan Yepes <yepes.j@hotmail.com>
2023-01-27 23:42:06 -0500

    valid parenthesis chall test

--- validParenthesis.ts
+++ validParenthesis.ts
@@ -2,9 +2,10 @@ const isValid = (string: string): boolean => {
   const map: { [key: string]: string } = { "(": ")", "{": "}", "[": "]" };
   const stack: string[] = [];
   for (const char of string) {
+    console.log(char);
     if (map[char]) {
       stack.push(char);
-    } else if (char !== map[stack[stack.length - 10]]) return false;
+    } else if (char !== map[stack[stack.length - 1]]) return false;
     else stack.pop();
   }
   return stack.length === 0;
@@ -14,17 +15,22 @@ const isValid2 = (string: string): boolean => {
   const map: { [key: string]: string } = { "(": ")", "{": "}", "[": "]" };
   const stack: string[] = [];
   for (const char of string) {
-    console.log(char);
     if (map[char]) {
       stack.push(char);
     } else {
       const last = stack[stack.length - 1];
+      console.log("char", char);
+      console.log(map[last]);
+      // console.log(last, char);
       if (last === undefined || char !== map[last]) return false;
-      else stack.pop();
+      else {
+        stack.pop();
+      }
     }
   }
+  console.log(stack.length === 0);
   return stack.length === 0;
 };
 console.time();
-console.log(isValid("({})"));
+console.log(isValid2("({)"));
 console.timeEnd();
