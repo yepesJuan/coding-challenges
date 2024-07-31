3a613cffa9e105917daeb1de53197c5cd5dcf0b2
Juan Yepes <yepes.j@hotmail.com>
2024-07-30 23:55:28 -0400

    feat: new chall longest common prefix

--- src/3.romanToInteger.ts
+++ src/3.romanToInteger.ts
@@ -1,3 +1,4 @@
+// In this function we use prevValue to determine if the previous num should be sub or add.
 function romanToInt(s: string): number {
   const romanIntegerMap: { [key: string]: number } = {
     I: 1,
@@ -14,7 +15,7 @@ function romanToInt(s: string): number {
 
   for (let i = 0; i < s.length; i++) {
     const curValue = romanIntegerMap[s[i]];
-    console.log(curValue);
+
 
     if (curValue > prevValue) {
       result += curValue - 2 * prevValue;
@@ -29,4 +30,35 @@ function romanToInt(s: string): number {
 }
 
 // console.log(romanToInt("LVIII"));
-console.log(romanToInt("MCMXCIV"));
+// console.log(romanToInt("MCMXCIV"));
+
+// In this function nextValue determines if the current numeral should be sub or add
+function romanToInt2(s: string): number {
+  const romanIntegerMap: { [key: string]: number } = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  let result = 0;
+  const length = s.length;
+
+  for (let i = 0; i < length; i++) {
+    const curValue = romanIntegerMap[s[i]];
+    const nextValue = i < length - 1 ? romanIntegerMap[s[i + 1]] : 0;
+
+    if (curValue < nextValue) {
+      result -= curValue;
+    } else {
+      result += curValue;
+    }
+  }
+
+  return result;
+}
+
+console.log(romanToInt2("MCMXCIV"))
