b0096a96ca148eda4ab175e8b9389b53e0ef9b4d
Juan Yepes <yepes.j@hotmail.com>
2024-07-31 23:54:37 -0400

    feat: longest commaon prefix part 2

--- src/4.longestCommonPrefix.ts
+++ src/4.longestCommonPrefix.ts
@@ -4,13 +4,35 @@ function longestCommonPrefix(strs: string[]): string {
   for (let i = 1; i < strs.length; i++) {
     while (!strs[i].startsWith(prefix)) {
       prefix = prefix.slice(0, -1);
-      if (prefix === '') return prefix;
+      if (prefix === "") return prefix;
     }
-
-  
   }
-  
-  return prefix;
-};
 
-console.log(longestCommonPrefix(["flower","flow","flight"]))
+  return prefix;
+}
+
+// console.log(longestCommonPrefix(["flower", "flow", "flight"]));
+
+
+function longestCommonPrefixVertical(strs: string[]): string {
+  if (strs.length === 0) return "";
+  const minLength = Math.min(...strs.map(str => str.length));
+
+  for (let i = 0; i < minLength; i++) {
+      const char = strs[0][i];
+      for (let j = 1; j < strs.length; j++) {
+        const a = strs[j]
+        const b = strs[j][i]
+        if (strs[j][i] !== char) {
+            const c = strs[0].slice(0, i)
+            const d = 0
+              return c;
+          }
+      }
+  }
+
+
+  return strs[0].slice(0, minLength);
+}
+
+console.log(longestCommonPrefixVertical(["flower", "flow", "flight"])); 
