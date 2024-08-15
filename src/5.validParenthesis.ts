be2e1e4ffe04477b82f317750eea4fadc1cfbfbd
Juan Yepes <yepes.j@hotmail.com>
2024-08-14 23:57:53 -0400

    fix: remove unnecesary vars

--- src/5.validParenthesis.ts
+++ src/5.validParenthesis.ts
@@ -25,9 +25,7 @@ const isValid = (string: string): boolean => {
   const map: { [key: string]: string } = { "(": ")", "{": "}", "[": "]" };
   const stack: string[] = [];
   for (const char of string) {
-    const a = map[char];
     if (map[char]) {
-      const b = map[stack[stack.length - 1]];
       stack.push(char);
     } else if (char !== map[stack[stack.length - 1]]) return false;
     else stack.pop();
